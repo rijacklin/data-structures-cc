31dbb0c82ef9421275752600258ab7cac28c57be
rijacklin <rijacklin1@gmail.com>
2023-12-05 13:20:19 -0500

    Completed Exercise 01-01

---
+++ ex01-01/main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+using namespace std;
+
+int main() {
+	ifstream file;
+	file.open("text.txt");
+
+	// Check if file is open for reading operation
+	if (!file.is_open()) {
+		std::cerr << "Unable to open file" << std::endl;
+		return 1;
+	}
+
+	string line;
+	set<string> set;
+
+	// Read lines from file and write to std output
+	while (getline(file, line)) {
+		set.insert(line);
+	}
+
+	for (std::set<string>::reverse_iterator it = set.rbegin(); it != set.rend(); ++it) {
+		std::cout << *it << std::endl;
+	}
+
+	// Close the file
+	file.close();
+	return 0;
+}
---
+++ notes.md
@@ -0,0 +1,28 @@
+# Chapter 1 Notes
+
+## C++ STD Template Library Classes
+
+### Queue
+
+- FIFO
+- [Reference](https://cplusplus.com/reference/queue/queue/)
+
+### Deque
+
+- Double-ended Queue
+- [Reference](https://cplusplus.com/reference/deque/deque/)
+
+### Stack
+
+- LIFO
+- [Reference](https://cplusplus.com/reference/stack/stack/)
+
+### SSet
+
+- Ordered Set
+- [Reference](https://cplusplus.com/reference/set/set/)
+
+### USSet
+
+- Unordered Set
+- [Reference](https://cplusplus.com/reference/unordered_set/unordered_set/)
