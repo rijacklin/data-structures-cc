2d56bd66b412fbca4594b2db6d1e913557b948a0
rijacklin <rijacklin1@gmail.com>
2023-12-06 15:47:45 -0500

    Completed Exercise 01-04

---
+++ ex01-04/test.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main() {
+	// Create a stack and push elments into it
+	stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+
+	// Create a queue
+	queue<int> q;
+
+	// Loop through the stack, push each element onto the queue, and pop
+	// the element off the stack
+	while (!s.empty()) {
+		q.push(s.top());
+		s.pop();
+	}
+
+	// Loop through the queue, print the elements in opposite order, and pop
+	// the elements off the queue
+	while (!q.empty()) {
+		s.push(q.front());
+		q.pop();
+	}
+
+	while (!s.empty()) {
+		std::cout << s.top() << std::endl;
+		s.pop();
+	}
+
+	return 0;
+}
